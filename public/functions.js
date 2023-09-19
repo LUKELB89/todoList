bc874767fc5592ea7b3ee49d4f6681944f1963cb
LUKELB89 <lukelbcoding89@gmail.com>
2023-09-19 11:42:50 -0300

    remove some comments

--- public/functions.js
+++ public/functions.js
@@ -1,26 +1,3 @@
-// function lineThrough() {
-//     const listItem = document.querySelectorAll('.item');
-
-//     listItem.forEach(li => {
-//         li.addEventListener('click', function (event) {
-//             if (event.target.tagName === 'LI') {
-//                 li.classList.toggle('lineThrough');
-//                 const taskId = li.getAttribute('data-task-id');
-//                 const isLineThrough = li.classList.contains('lineThrough');
-//                 localStorage.setItem(`task-${taskId}`, isLineThrough);
-//             }
-//         });
-
-//         const taskId = li.getAttribute('data-task-id');
-//         const isLineThrough = localStorage.getItem(`task-${taskId}`);
-//         if (isLineThrough === 'true') {
-//             li.classList.add('lineThrough');
-//         }
-//     });
-// }
-
-// document.addEventListener('DOMContentLoaded', lineThrough);
-
 function lineThrough() {
     const listItem = document.querySelectorAll('.item');
 
